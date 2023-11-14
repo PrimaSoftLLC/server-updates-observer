c75e3853564aed4b848389abc8f8d261584ff58c
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-14 14:45:38 +0300

    refactor ServerUpdateLoader

--- src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
+++ src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
@@ -4,7 +4,12 @@ import by.aurorasoft.updatesobserver.model.ServerUpdate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.List;
+
+import static by.aurorasoft.updatesobserver.util.InputStreamUtil.createObjectInputStream;
+import static by.aurorasoft.updatesobserver.util.InputStreamUtil.readObjects;
 
 @Component
 public final class ServerUpdateLoader {
@@ -15,8 +20,33 @@ public final class ServerUpdateLoader {
     }
 
     public List<ServerUpdate> load() {
-        try (final ServerUpdateDeserializer deserializer = new ServerUpdateDeserializer(this.updateFilePath)) {
-            return deserializer.deserialize();
+        try (final ObjectInputStream inputStream = createObjectInputStream(this.updateFilePath)) {
+            return readObjects(inputStream, ServerUpdate.class);
+        } catch (final IOException cause) {
+            throw new ServerUpdateLoadingException(cause);
         }
+    }
+
+    static final class ServerUpdateLoadingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException(final String description) {
+            super(description);
+        }
+
+        public ServerUpdateLoadingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
+
     }
 }
