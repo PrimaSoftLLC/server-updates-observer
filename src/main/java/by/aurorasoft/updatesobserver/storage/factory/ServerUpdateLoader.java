ce08f8c7c8e762e005a1892055b3fee7182be5f5
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-14 15:22:17 +0300

    refactor ServerUpdateLoader

--- src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
+++ src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
@@ -4,14 +4,9 @@ import by.aurorasoft.updatesobserver.model.ServerUpdate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.List;
 
-import static by.aurorasoft.updatesobserver.util.DeserializationUtil.createObjectInputStream;
 import static by.aurorasoft.updatesobserver.util.DeserializationUtil.readObjects;
-import static java.util.Collections.emptyList;
 
 @Component
 public final class ServerUpdateLoader {
@@ -22,35 +17,6 @@ public final class ServerUpdateLoader {
     }
 
     public List<ServerUpdate> load() {
-        try (final ObjectInputStream inputStream = createObjectInputStream(this.updateFilePath)) {
-            return readObjects(inputStream, ServerUpdate.class);
-        } catch (final EOFException exception) {
-            return emptyList();
-        } catch (final IOException cause) {
-            throw new ServerUpdateLoadingException(cause);
-        }
-    }
-
-    static final class ServerUpdateLoadingException extends RuntimeException {
-
-        @SuppressWarnings("unused")
-        public ServerUpdateLoadingException() {
-
-        }
-
-        @SuppressWarnings("unused")
-        public ServerUpdateLoadingException(final String description) {
-            super(description);
-        }
-
-        public ServerUpdateLoadingException(final Exception cause) {
-            super(cause);
-        }
-
-        @SuppressWarnings("unused")
-        public ServerUpdateLoadingException(final String description, final Exception cause) {
-            super(description, cause);
-        }
-
+        return readObjects(this.updateFilePath, ServerUpdate.class);
     }
 }
