4a2581f3f72b21f34142b206fb9ce9ce152a93ec
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-15 17:02:25 +0300

    refactor ServerUpdateLoader

--- src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
+++ src/main/java/by/aurorasoft/updatesobserver/storage/factory/ServerUpdateLoader.java
@@ -1,22 +1,57 @@
 package by.aurorasoft.updatesobserver.storage.factory;
 
 import by.aurorasoft.updatesobserver.model.ServerUpdate;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-import static by.aurorasoft.updatesobserver.util.DeserializationUtil.readObjects;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public final class ServerUpdateLoader {
-    private final String updateFilePath;
+    private static final TypeReference<Collection<ServerUpdate>> SERVER_UPDATES_TYPE_REFERENCE = new TypeReference<>() {
+    };
 
-    public ServerUpdateLoader(@Value("${server-updates.file-path}") final String updateFilePath) {
-        this.updateFilePath = updateFilePath;
+    private final ObjectMapper objectMapper;
+    private final File updateFile;
+
+    public ServerUpdateLoader(final ObjectMapper objectMapper,
+                              @Value("${server-updates.file-path}") final String updateFilePath) {
+        this.objectMapper = objectMapper;
+        this.updateFile = new File(updateFilePath);
     }
 
-    public List<ServerUpdate> load() {
-        return readObjects(this.updateFilePath, ServerUpdate.class);
+    public Collection<ServerUpdate> load() {
+        try {
+            return this.objectMapper.readValue(this.updateFile, SERVER_UPDATES_TYPE_REFERENCE);
+        } catch (final IOException cause) {
+            throw new ServerUpdateLoadingException(cause);
+        }
+    }
+
+    static final class ServerUpdateLoadingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException(final String description) {
+            super(description);
+        }
+
+        public ServerUpdateLoadingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public ServerUpdateLoadingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
+
     }
 }
