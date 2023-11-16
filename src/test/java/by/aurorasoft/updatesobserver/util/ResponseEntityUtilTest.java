4a39035f45e73043930410080dce04ae901173ff
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-16 16:30:27 +0300

    add ResponseEntityUtilTest

---
+++ src/test/java/by/aurorasoft/updatesobserver/util/ResponseEntityUtilTest.java
@@ -0,0 +1,35 @@
+package by.aurorasoft.updatesobserver.util;
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Duration;
+
+import static by.aurorasoft.updatesobserver.util.ResponseEntityUtil.noContent;
+import static by.aurorasoft.updatesobserver.util.ResponseEntityUtil.ok;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.http.CacheControl.maxAge;
+
+public final class ResponseEntityUtilTest {
+
+    @Test
+    public void noContentResponseEntityShouldBeCreated() {
+        final ResponseEntity<?> actual = noContent();
+        final ResponseEntity<?> expected = ResponseEntity.noContent().build();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void okResponseEntityShouldBeCreated() {
+        final Object givenBody = new Object();
+        final Duration givenMaxAge = Duration.of(1, SECONDS);
+
+        final ResponseEntity<Object> actual = ok(givenBody, givenMaxAge);
+        final ResponseEntity<Object> expected = ResponseEntity.ok()
+                .cacheControl(maxAge(givenMaxAge).cachePrivate())
+                .body(givenBody);
+        assertEquals(expected, actual);
+    }
+
+}
