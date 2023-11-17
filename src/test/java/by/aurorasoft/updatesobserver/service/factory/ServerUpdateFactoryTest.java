1800d71280aa898facc08902ada038d4a031d213
Vlad Zuev <vladzuev.00@mail.ru>
2023-11-17 11:29:34 +0300

    refactor ServerUpdateFactoryTest

--- src/test/java/by/aurorasoft/updatesobserver/service/factory/ServerUpdateFactoryTest.java
+++ src/test/java/by/aurorasoft/updatesobserver/service/factory/ServerUpdateFactoryTest.java
@@ -1,48 +1,39 @@
 package by.aurorasoft.updatesobserver.service.factory;
 
 import by.aurorasoft.updatesobserver.model.ServerUpdate;
-import by.aurorasoft.updatesobserver.util.InstantUtil;
 import org.junit.Test;
-import org.mockito.MockedStatic;
 
 import java.time.Instant;
 
-import static by.aurorasoft.updatesobserver.util.InstantUtil.plusMinutes;
-import static java.time.Instant.parse;
+import static java.time.Duration.between;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.same;
-import static org.mockito.Mockito.mockStatic;
+import static org.junit.Assert.assertNotNull;
 
 public final class ServerUpdateFactoryTest {
     private final ServerUpdateFactory factory = new ServerUpdateFactory();
 
     @Test
     public void updateShouldBeCreated() {
-        try (final MockedStatic<Instant> mockedStaticInstant = mockStatic(Instant.class);
-             final MockedStatic<InstantUtil> mockedStaticInstantUtil = mockStatic(InstantUtil.class)) {
-            final String givenServerName = "server";
-            final int givenDowntimeInMinutes = 5;
-            final int givenExtraLifetimeInMinutes = 10;
+        final String givenServerName = "server";
+        final long givenDowntimeInMinutes = 5;
+        final long givenExtraLifetimeInMinutes = 3;
 
-            final Instant givenNow = parse("2023-11-15T10:15:30.00Z");
-            mockedStaticInstant.when(Instant::now).thenReturn(givenNow);
+        final ServerUpdate actual = this.factory.create(
+                givenServerName,
+                givenDowntimeInMinutes,
+                givenExtraLifetimeInMinutes
+        );
 
-            final Instant givenDowntime = parse("2023-11-15T10:20:30.00Z");
-            mockedStaticInstantUtil.when(() -> plusMinutes(same(givenNow), eq(givenDowntimeInMinutes)))
-                    .thenReturn(givenDowntime);
+        final String actualServerName = actual.getServerName();
+        assertEquals(givenServerName, actualServerName);
 
-            final Instant givenLifetime = parse("2023-11-15T10:30:30.00Z");
-            mockedStaticInstantUtil.when(() -> plusMinutes(same(givenNow), eq(givenExtraLifetimeInMinutes)))
-                    .thenReturn(givenDowntime);
+        final Instant downtime = actual.getDowntime();
+        assertNotNull(downtime);
 
-            final ServerUpdate actual = this.factory.create(
-                    givenServerName,
-                    givenDowntimeInMinutes,
-                    givenExtraLifetimeInMinutes
-            );
-            final ServerUpdate expected = new ServerUpdate(givenServerName, givenDowntime, givenLifetime);
-            assertEquals(expected, actual);
-        }
+        final Instant lifetime = actual.getLifetime();
+        assertNotNull(lifetime);
+
+        final long actualMinuteDifferenceDowntimeAndLifetime = between(downtime, lifetime).toMinutes();
+        assertEquals(givenExtraLifetimeInMinutes, actualMinuteDifferenceDowntimeAndLifetime);
     }
 }
